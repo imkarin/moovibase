b28e5f7d863ea3c04e128c9337d902961949dafc
Karin Meijvogel <karin@axeos.com>
2022-04-25 23:25:05 +0200

    Refresher: Manually make files in src folder

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
 
-
-function App() {
-  // State: [name of the state, setter-function] = useState(initialvalue);
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert('Counter changed to ' + counter)
-  }, [counter]) // 2nd arg: dependency array. "only rerun if counter changes"
-  // the dependency array can be left empty: the effect will run only once (at initial load of component)
-
+const App = () => {
   return (
-    <div className="App">
-      <button onClick={() => {
-        setCounter((prevCount) => prevCount + 1)
-      }}>+</button>
-      
-      <h1>{counter}</h1>
-
-      <button onClick={() => {
-        setCounter((prevCount) => prevCount - 1)
-      }}>-</button>
-    </div>
+    <h1>Movies!</h1>
   );
 }
 
