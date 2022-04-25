261d1680f17f752d35053bf02174482b7a8fe549
Karin Meijvogel <karin@axeos.com>
2022-04-25 17:20:54 +0200

    Refresher: components and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
 
+// Custom component
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.firstName}</h1>
+      <h2>Last name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
 function App() {
+  const name = 'John';
+
   return (
     <div className="App">
-      <h1>Goodbye World</h1>
+      <Person 
+        firstName='John' 
+        lastName='Doe' 
+        age={27} 
+      />
+      <Person 
+        firstName='Danny' 
+        lastName="O'Donoghue" 
+        age={25} 
+      />
     </div>
   );
 }
