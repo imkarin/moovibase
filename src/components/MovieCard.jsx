768017cb7083f713eed4694377c23000ff8867d1
Karin Meijvogel <karin@axeos.com>
2022-04-26 17:03:16 +0200

    Create MovieCard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MovieCard = (props) => {
+  const movie = props.movie;
+
+  return (
+    <div className='movie'>
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+      <figure>
+        <img src={ movie.Poster !== 'N/A' ? movie.Poster : 'http://forkliftsystems.com.au/wp-content/uploads/2015/04/placeholder-600x400-400x400.png'} alt={`${movie.Title} poster`} />
+        <figcaption>
+          <p>{movie.Type}</p>
+          <h3>{movie.Title}</h3>
+        </figcaption>
+      </figure>
+    </div>
+  )
+}
+
+export default MovieCard
